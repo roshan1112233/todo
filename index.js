4900fcc1d4fe0255c8bff5ee37d49be35ae4d427
roshan <roshanchava4@gmail.com>
2025-05-08 14:37:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+let todos = [];
+app.get('/', (req, res) => res.send('To-Do App Live!'));
+app.get('/todos', (req, res) => res.json(todos));
+app.post('/todos', (req, res) => {
+  todos.push(req.body.todo);
+  res.json({ message: 'Todo added' });
+});
+app.listen(3000, () => console.log('Server on port 3000'));
